83d0b78e941c7ce8c959adb9c1a2598a14f90aad
airestoy17 <alinares30@alumnos.uaq.mx>
2022-05-29 14:19:30 -0500

    aaaa

--- php/update/Umultas.php
+++ php/update/Umultas.php
@@ -17,11 +17,74 @@ Desconectar($Con);
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Update Multas</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
 </head>
 
 <body>
+    <div class="container">
+        <div class="py-5 text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="d-block mx-auto mb-4" width="72" height="72" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
+            </svg>
+            <h2>Edite una multa</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 order-md-1">
+                <h4 class="mb-3">Folio</h4>
+                <form class="needs-validation" method="get" action="Ipropietario.php" novalidate>
+                    <div class="mb-3">
+                        <label for="folio">Folio</label>
+                        <input type="text" class="form-control" id="folio" placeholder="" value="<?php print($Fila[1]); ?>" required />
+                        <div class="invalid-feedback">Valid folio is required.</div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="clave">clave</label>
+                        <input type="text" class="form-control" id="folio" placeholder="" value="<?php print($Fila[4]); ?>"/>
+                        <div class="invalid-feedback">
+                            Valid folio is required.
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="motivo">Motivo</label>
+                            <input type="text" class="form-control" id="motivo" placeholder="Querétaro" value="<?php print($Fila[2]); ?>"/>
+                            <div class="invalid-feedback">
+                                Valid Motivo is requied.
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="fundamento">Fundamento</label>
+                            <input type="text" class="form-control" id="fundamento" placeholder="Milenio III" value="<?php print($Fila[3]); ?>"/>
+                            <div class="invalid-feedback">
+                                Valid Fundamento is required.
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="mb-4" />
+                    <button class="btn btn-primary btn-lg btn-block" type="submit">
+                        Continue to checkout
+                    </button>
+                </form>
+            </div>
+        </div>
+
+        
+
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+            <p class="mb-1">&copy; Sistema de control vehicular</p>
+        </footer>
+    </div>
+<!--
     <form action="Umultas.php" id="form">
         <label for="Folio">Folio</label>
         <input type="text" name="Folio" id="Folio" value='<?php print($Fila[0]); ?>' >
@@ -70,6 +133,7 @@ Desconectar($Con);
         <br>
         <button type="submit">Subir</button>
     </form>
+-->
 </body>
 
 </html>
