bf1cfa879cbe1950702e678e6bf4189fc73a0da9
Alfredo Vanegas <thealf154@gmail.com>
2022-06-01 15:28:04 -0500

    Final

--- php/add/Ilicencias.php
+++ php/add/Ilicencias.php
@@ -14,7 +14,6 @@ print("ID:" . $ID . "</br>");
 
 $intervalo = strval($vigencia) . " years";
 $date = date_create($FechaExp);
-print($intervalo);
 $FechaVen = date('Y-m-d', strtotime($FechaExp. " + $vigencia years"));
 
 include("Conexion.php");
@@ -26,4 +25,4 @@ $Result = Ejecutar($Con, $SQL);
 print("\nResult: " . $Result);
 Desconectar($Con);
 
-//print('<META HTTP-EQUIV="REFRESH" CONTENT="1;URL=/php/see/CIlicencias.php">');
+print('<META HTTP-EQUIV="REFRESH" CONTENT="1;URL=/proyecto_final/php/see/CIlicencias.php">');
--- php/update/Ulicencias.php
+++ php/update/Ulicencias.php
@@ -172,5 +172,6 @@ if (isset($_GET['tipo'])) {
   $Result = Ejecutar($Con, $SQL);
   print("Registros actualizados = " . mysqli_affected_rows($Con));
   Desconectar($Con);
+  print('<META HTTP-EQUIV="REFRESH" CONTENT="1;URL=/proyecto_final/php/see/CIlicencias.php">');
 }
 ?>
